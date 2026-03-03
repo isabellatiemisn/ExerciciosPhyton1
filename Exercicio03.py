bb9bc4baaac2ccb7be3f9ba26c02641b5ee0357c
isabellatiemisn <isabella.tiemi.sn@gmail.com>
2026-03-03 10:30:01 -0300

    Update and rename Exercicio3.py to Exercicio03.py

--- Exercicio03.py
+++ Exercicio03.py
@@ -7,4 +7,5 @@ Base = float(input('Coloque o valor da base para descobrir a área do triângulo
 Altura = float(input('Coloque o valor da altura para descobrir a área do triângulo:'))
 Area = ((Base*Altura)/2)
 print ('Essa é a área do triângulo: ', Area)
+
 #Fim
