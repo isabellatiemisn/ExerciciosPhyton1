30b7e1dfcccf242815d783708f9f62b91e9291e7
isabellatiemisn <isabella.tiemi.sn@gmail.com>
2026-03-03 10:30:24 -0300

    Update and rename Exercicio4.py to Exercicio04.py

--- Exercicio04.py
+++ Exercicio04.py
@@ -5,4 +5,5 @@ Fahrenheit: float = 0.0
 Celsius = float(input('Coloque o valor em celsius para converter para fahrenheit:'))
 Fahrenheit = ((9*Celsius)+160)/5
 print ('Aqui está o valor em fahrenheit:', Fahrenheit)
+
 #Fim
